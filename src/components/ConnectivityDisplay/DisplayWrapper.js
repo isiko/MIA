2c232451ae411b4dfc841ae77574bd192fc45ab5
isiko404 <isihd.ko@gmail.com>
2022-08-16 10:14:53 +0200

    Add Battery Status Plugin

--- src/components/ConnectivityDisplay/DisplayWrapper.js
+++ src/components/ConnectivityDisplay/DisplayWrapper.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function ConnectionDisplayWrapper({children, active}) {
+export default function ConnectionDisplayWrapper({children, active, title}) {
   return (
-    <div className={`connectivity-wraper ${active === undefined ? null : active ? 'connectivity-wraper-active' : 'connectivity-wraper-inactive'}`}>
+    <div title={title} className={`connectivity-wraper ${active === undefined ? null : active ? 'connectivity-wraper-active' : 'connectivity-wraper-inactive'}`}>
         {children}
     </div>
   )
